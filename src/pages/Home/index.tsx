2a7ecac2d18b7e5045dc16a19bc2b56c368a65d5
André Araujo <andre.cristiano1975@gmail.com>
2021-04-17 18:21:53 +0000

    Final commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,21 +23,23 @@ interface CartItemsAmount {
 
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    const newSumAmount = {...sumAmount};
+    newSumAmount[product.id] = product.amount;
+    
+    return newSumAmount;
+    
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      const { data } = await api.get('products');
+      const { data } = await api.get<Product[]>('products');
 
-      console.log(data);
+      const products: ProductFormatted[] = data.map(product => ({...product, priceFormatted: formatPrice(product.price)}))
 
-      //const products: ProductFormatted[] = data.map(product => product.)
-
-      setProducts(data);
+      setProducts(products);
 
     }
 
@@ -45,7 +47,7 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id);
   }
 
   return (
@@ -58,11 +60,11 @@ const Home = (): JSX.Element => {
           <button
             type="button"
             data-testid="add-product-button"
-          // onClick={() => handleAddProduct(product.id)}
+            onClick={() => handleAddProduct(product.id)}
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
+              {cartItemsAmount[product.id] || 0}
           </div>
 
             <span>ADICIONAR AO CARRINHO</span>
